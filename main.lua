3d9bdb832c73aed19713e62b99fe29cf834283ee
knowngecko <knowngecko@protonmail.com>
2024-11-01 18:36:28 +0000

    Added time keeping, now tells you how long each core took

--- main.lua
+++ main.lua
@@ -1,6 +1,13 @@
-local relative_script_path = debug.getinfo(1, "S").source:sub(2):match("(.*/)") or "./"
-package.path = package.path .. ";" ..  relative_script_path .. "?.lua";
+local RelativeScriptPath = debug.getinfo(1, "S").source:sub(2):match("(.*/)") or "./"
+package.path = package.path .. ";" ..  RelativeScriptPath .. "?.lua";
 local Common = require("common");
+
+local DateCommand = "date +%s%N | cut -b1-13"
+local TotalStartTime = Common.execute_command(DateCommand);
+
+local function round_to_2dp(Number)
+    return tonumber(string.format("%.2f", Number))
+end
 
 --> Decipher argument path
 local FileName = "packages";
@@ -19,11 +26,14 @@ if Configuration.Settings.SuperuserCommand ~= "" then Configuration.Settings.Sup
 
 print(Colours.Blue.. "[ENTER] Beginning".. Colours.Reset);
 for Index, Value in pairs(Configuration["Settings"]["Cores"]) do
+    local StartTime = Common.execute_command(DateCommand);
     Value = string.lower(Value);
     local Core = require("cores/"..Value.."/"..Value);
     Value = Value:gsub("^%l", string.upper)
     print(Colours.Magenta.. Colours.Bold.. "[LOG] Executing: ".. Value.. " Core".. Colours.Reset)
     Core.execute(Configuration);
-    print(Colours.Magenta.. Colours.Bold.. "[LOG] Completed: ".. Value.. " Core".. Colours.Reset)
+    local EndTime = Common.execute_command(DateCommand);
+    print(Colours.Magenta.. Colours.Bold.. "[LOG] Completed: ".. Value.. " Core in ".. round_to_2dp((EndTime - StartTime) / 1000).. "s" .. Colours.Reset)
 end
-print(Colours.Blue.. "[EXIT] Finished".. Colours.Reset);
+local TotalEndTime = Common.execute_command(DateCommand);
+print(Colours.Blue.. "[EXIT] Finished: " ..round_to_2dp((TotalEndTime - TotalStartTime) / 1000).."s".. Colours.Reset);
